f7fc0289f1c95811652653c72d5c3a8427717169
Kyle <kylefink3@gmail.com>
2025-03-23 18:50:23 -0400

    Working web not working phone

--- Megan.js
+++ Megan.js
@@ -16,6 +16,23 @@ $(document).ready(function() {
     function open() {
         envelope.addClass("open")
             .removeClass("close");
+
+            for (let i = 0; i < 10; i++) {
+                let heart = document.createElement('div');
+                heart.innerHTML = '❤️';
+                heart.classList.add('heartb');
+                document.body.appendChild(heart);
+                heart.style.left = `${Math.random() * 100}vw`;
+                heart.style.top = '50%';
+                heart.style.opacity = '1';
+                heart.style.transform = `translateY(-${Math.random() * 200 + 100}px)`;
+                heart.style.transition = 'opacity 1s ease-in-out, transform 2s ease-in-out';
+                
+                setTimeout(() => {
+                    heart.style.opacity = '0';
+                    setTimeout(() => heart.remove(), 2000);
+                }, 100);
+            }
     }
 
     function close() {
